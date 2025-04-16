5da679d622d295b1fb9f5491bb08782b0a5372d4
maks-plaksa <makslehtman@gmail.com>
2025-04-17 02:53:48 +0300

    Homework 7.2 v2

--- homeWork7.2.js
+++ homeWork7.2.js
@@ -1,6 +1,7 @@
 
-const words = 'Happiness, Time, Task, Wonderful, Joyful Apple';
-const pattern = /^[b-zB-Z]{6,}$/g; // Matches words with 6 or more characters, excluding 'a' and 'A'
+const words = 'Happiness, Time, Task, Wonderful, Happiness, Joyful, Apple';
+// const pattern = /[B-Z][b-z]{5,}/g;
+const pattern = /\b[B-Zb-z]{6,}\b/g;
 
 let matches = words.match(pattern); 
 
