d350cbdae31fb52ef14b3d4450ac8cac23069af8
maks-plaksa <makslehtman@gmail.com>
2025-04-17 02:07:07 +0300

    Homework 7.1

--- homeWork7.1.js
+++ homeWork7.1.js
@@ -1,50 +1,30 @@
-var services = {
-    "Стрижка": "60 грн",
-    "Бритье": "80 грн",
-    "Вымыть голову": "100 грн",
-    
-    price: function () {
-        var total = 0;
-        for (var key in this) {
-            if (typeof this[key] === 'string' && this[key].includes('грн')) {
-                total += parseInt(this[key]);
-            }
-        }
-        return total + ' грн';
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
     },
-    
-    minPrice(){
-        var servicesArray = Object.values(this).filter(value => typeof value === 'string');
-
-        servicesArray.sort((a, b) => {
-            const priceA = parseInt(a);
-            const priceB = parseInt(b);
-            return priceA - priceB;
-        });
-
-        return servicesArray[0];
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
     },
-    
-    maxPrice(){
-        var servicesArray = Object.values(this).filter(value => typeof value === 'string');
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+];
 
-        servicesArray.sort((a, b) => {
-            const priceA = parseInt(a);
-            const priceB = parseInt(b);
-            return priceA - priceB;
-        });
+// const pattern = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-zA-Z]{2,}$/; // регулярний вираз для перевірки email
+const pattern = /^[a-z0-9]+\.?([a-z0-9]+)?@(gmail.com)|(yahoo.com)$/;
 
-        return servicesArray[servicesArray.length - 1];
+let validEmails = [];
 
-
+arr.forEach(element => {
+    if (element.email.match(pattern)) {
+        validEmails.push(element.email);
     }
-    };
+});
 
-
-services['Заменить стекло'] = "200 грн";
-services['Разбить стекло'] = "20 грн";
-
-
-console.log('Min:' + services.minPrice());
-console.log('Max:' + services.maxPrice());
-console.log('Total:' + services.price());
+console.log(validEmails);
